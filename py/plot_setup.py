464e4c9d1d02736e6f45109f187b5c3b7df09e79
Chandan <cs3hq@virginia.edu>
2017-03-20 11:20:47 -0400

    simple velocity v. nodal diameter plot

---
+++ py/plot_setup.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+from matplotlib import rc
+rc('lines', markersize=4)
+rc('legend', numpoints=1, fontsize=10)
+rc('axes', labelsize=10)
+rc('lines', markeredgewidth=0)
+rc('axes', color_cycle=['#E24A33', '#348ABD', '#988ED5', '#777777', '#FBC15E',
+                        '#8EBA42', '#FFB5B8', 'red', 'green', 'blue', 'purple', 'cyan', 'black', 'brown'])
+rc('xtick', labelsize=7)
+rc('ytick', labelsize=7)
+plt.hold(True)
+path = '/Users/chandansingh/drive/asdf/research/singh_connectome/'
+
+import matplotlib
+matplotlib.rcParams['mathtext.fontset'] = 'custom'
+matplotlib.rcParams['mathtext.rm'] = 'Bitstream Vera Sans'
+matplotlib.rcParams['mathtext.it'] = 'Bitstream Vera Sans:italic'
+matplotlib.rcParams['mathtext.bf'] = 'Bitstream Vera Sans:bold'
---
+++ py/plot_velocity.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import plot_setup
+
+fname = '../data/Dbl_Taper_20uDF_1.txt'
+x = np.loadtxt(fname, skiprows=1)
+headers = np.genfromtxt(fname, dtype="str")[0, :]
+data = {}
+for i in range(len(headers)):
+    data[headers[i]] = x[:, i]
+plt.plot(data['Dn'], data['ave_velocity'])
+plt.xlabel("Nodal Diameter")
+plt.ylabel("Velocity")
+plt.savefig("../plots/velocity.pdf")
+plt.show()
