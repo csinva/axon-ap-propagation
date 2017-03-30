32d8571dcb75444c67636e747c119668f6394b00
Chandan <cs3hq@virginia.edu>
2017-03-30 18:14:05 -0400

    plot energy, velocity, diam

--- py/plot_velocity.py
+++ py/plot_velocity.py
@@ -2,14 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plot_setup
 
-fname = '../data/Dbl_Taper_20uDF.txt'
+fname = '../data/out.txt'
 x = np.loadtxt(fname, skiprows=1)
 headers = np.genfromtxt(fname, dtype="str")[0, :]
 data = {}
 for i in range(len(headers)):
     data[headers[i]] = x[:, i]
+print("keys", data.keys())
+ROWS=1
+COLS=3
+plt.subplot(ROWS,COLS,1)
 plt.plot(data['nodal_diameter'], data['ave_velocity'])
 plt.xlabel("Nodal Diameter")
-plt.ylabel("Velocity")
-plt.savefig("../plots/velocity.pdf")
+plt.ylabel("AP Velocity")
+
+plt.subplot(ROWS,COLS,2)
+plt.plot(data['nodal_diameter'], data['na_flux'],'o',label="Na flux")
+plt.plot(data['nodal_diameter'], data['k_flux'],'o',label="K flux")
+plt.plot(data['nodal_diameter'], data['na_flux']+data['k_flux'],'o',label="Total flux")
+plt.xlabel("Nodal Diameter")
+plt.ylabel("AP Costs")
+plt.legend()
+
+plt.subplot(ROWS,COLS,3)
+plt.plot(data['ave_velocity'], data['na_flux'],'o',label="Na flux")
+plt.plot(data['ave_velocity'], data['k_flux'],'o',label="K flux")
+plt.plot(data['ave_velocity'], data['na_flux']+data['k_flux'],'o',label="Total flux")
+plt.xlabel("AP Velocity")
+plt.ylabel("AP Costs")
+plt.legend()
+
+plt.savefig("../plots/energy_velocity_diameter.pdf")
 plt.show()
